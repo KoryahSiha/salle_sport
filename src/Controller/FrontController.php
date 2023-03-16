5ecae2bfe83d1fd06bcee9877ae6c00b347424d5
KoryahSiha <koryahsiha@gmail.com>
2023-03-16 13:39:08 +0100

    Ajout des routes dans FrontController
    Les routes '/', '/cours', '/contact' ont été ajoutées.

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,6 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/cours', name: 'app_course')]
+    public function course(): Response
+    {
+        return $this->render('course.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
     #[Route('/front', name: 'app_front')]
     public function index(): Response
     {
